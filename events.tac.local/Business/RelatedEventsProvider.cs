48faa41cb7d4208fb073a331138f7d9970cf354e
Benoit Quette <benoit.quette@gmail.com>
2018-05-17 10:50:59 +0200

    Module 7

---
+++ events.tac.local/Business/RelatedEventsProvider.cs
@@ -0,0 +1,32 @@
+﻿using events.tac.local.Models;
+using Sitecore.Data.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TAC.Sitecore.Abstractions.Interfaces;
+using TAC.Sitecore.Abstractions.SitecoreImplementation;
+
+namespace events.tac.local.Business
+{
+    public class RelatedEventsProvider
+    {
+        private readonly IRenderingContext renderingContext;
+        public RelatedEventsProvider() : this(SitecoreRenderingContext.Create()) { }
+        public RelatedEventsProvider(IRenderingContext renderingContext)
+        {
+            this.renderingContext = renderingContext;
+        }
+
+        public IEnumerable<NavigationItem> GetRelatedEvents()
+        {
+            return this.renderingContext
+                .ContextItem
+                .GetMultilistFieldItems("RelatedEvents")
+                .Select(item => new NavigationItem(
+                    title: item.DisplayName,
+                    uRL: item.Url));
+            
+        }
+    }
+}
---
+++ events.tac.local/Controllers/RelatedEventsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using events.tac.local.Business;
+
+namespace events.tac.local.Controllers
+{
+    public class RelatedEventsController : Controller
+    {
+        private RelatedEventsProvider provider;
+
+        public RelatedEventsController() : this(new RelatedEventsProvider()) { }
+
+        public RelatedEventsController(RelatedEventsProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        // GET: RelatedEvents
+        public ActionResult Index()
+        {
+            return View(this.provider.GetRelatedEvents());
+        }
+    }
+}
